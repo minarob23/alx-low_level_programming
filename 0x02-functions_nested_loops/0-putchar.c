9257a20ef10b0ce9a40b847d7e5a994014cb70e0
minarob23 <menarobir246rm@gmail.com>
2023-03-21 06:03:32 -0700

    0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+/**
+ * main - Entry point
+ * Description: 'prints _putchar to stdout'
+ * Return:  Always 0
+ */
+int main(void)
+{
+	_putchar('-');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+}
