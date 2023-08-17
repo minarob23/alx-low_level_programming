f22da153009ffd2fd4b3ddf2aca3e9105ef8a216
Mina Robir <menarobir246rm@gmail.com>
2023-08-18 02:50:55 +0300

    Update 1-dlistint_len.c

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -8,9 +8,12 @@
  */
 size_t dlistint_len(const dlistint_t *h) {
     size_t count = 0;
+    const dlistint_t *current = h;
 
-    for (; h != NULL; h = h->next) {
+    while (current != NULL) {
         count++;
+        current = current->next;
     }
+
     return count;
 }
